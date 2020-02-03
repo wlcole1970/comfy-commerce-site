e2a103907207fa66f1f5a85459d7d1a19048cc67
Walter Cole <waltercole@Walters-MacBook-Pro-2.local>
2020-02-02 21:45:20 -0600

    added cart functionality

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const productsDOM = document.querySelector('.products-center');
 let cart = [];
 
 //buttons
-let buttonsDOM[];
+let buttonsDOM = [];
 
 //getting products
 
@@ -57,6 +57,8 @@ class UI {
 		let result = '';
 		products.forEach(product => {
 			result +=
+
+				//taken from index.html
 				`
             <article class="product">
                 <div class="img-container">
@@ -93,21 +95,62 @@ class UI {
 				event.target.innerText = "In Cart";
 				event.target.disabled = true;
 				// get product from products based on .id
+				let cartItem = { ...Storage.getProduct(id), amount: 1 };
+
 
 				//add product to carts
+				cart = [...cart, cartItem];
 
 				//cart saved to local storage
-
+				Storage.saveCart(cart);
 				//set cart values
-
+				this.setCartValues(cart);
 
 				//display cart item
-
+				this.addCartItem(cartItem);
 				//show cart
+				this.showCart();
+
 			});
 			//}
 		});
 	}
+	setCartValues(cart) {
+		let tempTotal = 0;
+		let itemsTotal = 0;
+		cart.map(item => {
+			tempTotal += item.price * item.amount;
+			itemsTotal += item.amount
+		})
+
+		cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+		cartItems.innerText = itemsTotal;
+	}
+	addCartItem(item) {
+		const div = document.createElement('div');
+		div.classList.add('cart-item');
+		div.innerHTML = `
+		<img src=${item.image} alt="product">
+		<div>
+			<h4>${item.title}</h4>
+			<h5>$${item.price}</h5>
+			<span class="remove-item">remove</span>
+		</div>
+		<div>
+			<i class="fas fa-chevron-up" data-id=${item.id}></i>
+			<p class="item-amount">${item.amount}</p>
+			<i class="fas fa-chevron-down" data-id=${item.id}></i>
+		</div>
+		`;
+		cartContent.appendChild(div);
+
+	}
+
+	showCart() {
+		cartOverlay.classList.add('transparentBcg');
+		cartDOM.classList.add('showCart');
+	}
+
 }
 
 //local storage
@@ -116,7 +159,13 @@ class Storage {
 	static saveProducts(products) {
 		localStorage.setItem("products", JSON.stringify(products));
 	}
-
+	static getProduct(id) {
+		let products = JSON.parse(localStorage.getItem('products'));
+		return products.find(product => product.id === id);
+	}
+	static saveCart(cart) {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
