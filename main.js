1827edf74c88dd5cbee0fcfd2d4336f248fc2d62
Walter Cole <waltercole@Walters-MacBook-Pro-2.local>
2020-02-04 23:45:59 -0600

    added increase/descrease amount to cart

--- main.js
+++ main.js
@@ -168,10 +168,41 @@ class UI {
 			this.clearCart();
 		});
 		//cart functionality
+		cartContent.addEventListener('click', (event) => {
+			if (event.target.classList.contains('remove-item')) {
+				let removeItem = event.target;
+				let id = removeItem.dataset.id;
+				cartContent.removeChild(removeItem.parentElement.parentElement);
+				this.removeItem(id);
+			} else if (event.target.classList.contains('fa-chevron-up')) {
+				let addAmount = event.target;
+				let id = addAmount.dataset.id;
+				let tempItem = cart.find((item) => item.id === id);
+				tempItem.amount = tempItem.amount + 1;
+				Storage.saveCart(cart);
+				this.setCartValues(cart);
+				addAmount.nextElementSibling.innerText = tempItem.amount;
+			} else if (event.target.classList.contains('fa-chevron-down')) {
+				let minusAmount = event.target;
+				let id = minusAmount.dataset.id;
+				let tempItem = cart.find((item) => item.id === id);
+				tempItem.amount = tempItem.amount - 1;
+				//lower than 0
+				if (tempItem.amount > 0) {
+					Storage.saveCart(cart);
+					this.setCartValues(cart);
+					minusAmount.previousElementSibling.innerText = tempItem.amount;
+				} else {
+					cartContent.removeChild(minusAmount.parentElement.parentElement);
+					this.removeItem(id);
+				}
+			}
+		});
 	}
 	clearCart() {
 		let cartItems = cart.map((item) => item.id);
 		cartItems.forEach((id) => this.removeItem(id));
+
 		while (cartContent.children.length > 0) {
 			cartContent.removeChild(cartContent.children[0]);
 		}
