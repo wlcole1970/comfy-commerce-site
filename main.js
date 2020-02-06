4ed479bd6e750a6616d56aaac34f707b8554dea2
Walter Cole <waltercole@Walters-MacBook-Pro-2.local>
2020-02-05 21:32:49 -0600

    minor changes

--- main.js
+++ main.js
@@ -172,7 +172,7 @@ class UI {
 			if (event.target.classList.contains('remove-item')) {
 				let removeItem = event.target;
 				let id = removeItem.dataset.id;
-				cartContent.removeChild(removeItem.parentElement.parentElement);
+				console.log(removeItem.parentElement.parentElement);
 				this.removeItem(id);
 			} else if (event.target.classList.contains('fa-chevron-up')) {
 				let addAmount = event.target;
